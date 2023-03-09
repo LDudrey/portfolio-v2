95d285584860b6883fffd0fa2ef2b4c65b763bee
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-03-08 22:22:12 -0600

    added imports to App.js code

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
+import React from "react";
 import Navigation from './components/Navigation';
+import Header from './components/Header';
+import Project from './components/Project';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="portfolio-app">
+    <div>
       <Navigation />
+      <Header />
+      <Project />
+      <Footer />
     </div>
   );
 }
