379e3093c479c95f7c6cae3ad84a6394e2064ece
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-03-10 12:34:22 -0600

    added styles to app.js, FA is now rendering icons on page and being styled

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React from "react";
-import PageContainer from "./components/PageContainer";
-// import Header from './components/elements/Header';
-// import Footer from './components/elements/Footer';
+import './styles/App.css';
+import PageContainer from "./components/PageContainer"
+import Header from "./components/elements/Header"
+import Footer from "./components/elements/Footer"
 
 function App() {
   return (
     <div>
-      {/* <Header /> */}
-      <PageContainer />;
-      {/* <Footer /> */}
+      <Header />
+      <PageContainer />
+      <Footer />
     </div>
   );
 }
+
+
 
 export default App;
--- src/components/elements/Footer.js
+++ src/components/elements/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
+
 
 export default function Footer() {
     return (
         <div>
-            
+            <footer>
+            <FontAwesomeIcon icon={faGithub} />
+            <FontAwesomeIcon icon={faLinkedin} />
+            <FontAwesomeIcon icon={faStackOverflow} />
+            </footer>
         </div>
     )
-}
+} 
