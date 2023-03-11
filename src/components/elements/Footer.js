4f8dc956a2d462d43d1ca4f8c347d8b297539a1a
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-03-11 03:30:27 -0600

    icons render and are styled with spacing

--- src/components/elements/Footer.js
+++ src/components/elements/Footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/Footer.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-svg-icons'
 
@@ -6,10 +7,10 @@ import { faGithub, faLinkedin, faStackOverflow } from '@fortawesome/free-brands-
 export default function Footer() {
     return (
         <div>
-            <footer>
-            <FontAwesomeIcon icon={faGithub} />
-            <FontAwesomeIcon icon={faLinkedin} />
-            <FontAwesomeIcon icon={faStackOverflow} />
+            <footer className="d-flex justify-content-center">
+            <a className="mx-3" href="https://github.com/LDudrey"><FontAwesomeIcon icon={faGithub} size="3x"/></a>
+            <a className="mx-3" href="https://www.linkedin.com/in/lydiadudrey/"><FontAwesomeIcon icon={faLinkedin} size="3x" /></a>
+            <a className="mx-3" href="https://stackoverflow.com/users/21372860/ldudrey"><FontAwesomeIcon icon={faStackOverflow} size="3x" /></a>
             </footer>
         </div>
     )
