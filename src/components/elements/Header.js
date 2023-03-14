3b0438f140e91c455feb1661c7f3d13c16135cb0
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-03-14 15:09:39 -0500

    moved header code over to main

--- src/components/elements/Header.js
+++ src/components/elements/Header.js
@@ -1,15 +1,19 @@
 import React from 'react';
+import image from '../../assets/images/avataaars.png'
 
 export default function Header() {
     return (
-        <a className='navbar-brand mb-lg-auto mt-lg-4' href="true">
+        <div>
+            <a className='navbar-brand m-0' href="#">
             <span className=''>
-                <img className="img-fluid" alt="cartoonized profile" src="images/avataaars.png"></img>
+                <img className="img-fluid" id="profile" alt="cartoonized profile" src={image}></img>
             </span>
-            <h1 className='text-center'>Lydia Dudrey</h1>
-            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            </a>
+            <h1 className='text-center my-3'>Lydia Dudrey</h1>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
-        </a>
+        </div>
     )
 }
