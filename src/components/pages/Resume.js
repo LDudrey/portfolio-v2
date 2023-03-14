86cbbbb133a327e3bc785a5fe38ab091888ae46c
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-03-14 15:08:11 -0500

    moved resume code over to main

--- src/components/pages/Resume.js
+++ src/components/pages/Resume.js
@@ -1,17 +1,15 @@
 import React from 'react';
+import resume from '../../assets/files/LydiaDudreyResume.pdf'
 
 export default function Resume() {
   return (
-    <div id="resume" className='col'>
+    <div id="resume" className='col vw-100 bg-dark'>
       <h1>Resume</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Download my 
+        <a href={resume}> Resume</a>
       </p>
+
     </div>
   );
 }
