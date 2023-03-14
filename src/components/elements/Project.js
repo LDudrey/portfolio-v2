dbe829291b1631df5e4fcc99196c17c7a03b5ae4
LDudrey <113869466+LDudrey@users.noreply.github.com>
2023-03-14 15:10:25 -0500

    add project code to main

--- src/components/elements/Project.js
+++ src/components/elements/Project.js
@@ -2,20 +2,19 @@ import React from 'react';
 
 export default function ProjectCard(props) {
     return (
-        <div className="project-card">
+        <div className="project-list">
             {props.projects.map((item) => {
                 return (
-                    <div className="card" key={item.id}>
+                    <div className="card m-5" key={item.id}>
                         <img src={item.image} className="card-img-top" alt={item.alt}></img>
                         <div className="card-body">
                             <h5 className="card-title">{item.name}</h5>
                             <p className="card-text">{item.description}</p>
-                        </div>
-                        <div className="card-body">
-                            <a href={item.link} className="card-link">Card link</a>
+                            <a href={item.link} className="card-link">Visit</a>
                         </div>
                     </div>
-                )}
+                )
+            }
             )}
         </div>
     );
